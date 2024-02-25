560b3cfd6bb0d112e05536953e48e40842a94486
Zioun <jionkhan0@gmail.com>
2024-02-25 10:28:55 +0600

    added

---
+++ arrow-function.js
@@ -0,0 +1,29 @@
+// TODO function declaration
+/*
+
+function add(a, b){
+    const result = a + b;
+    return result
+}
+const result = add(10, 20)
+console.log(result);
+
+*/
+
+// TODO function expression
+/*
+
+const add = function(a, b){
+    const result = a + b;
+    return result;
+}
+const result = add(10, 20);
+console.log(result);
+
+*/
+
+// TODO arrow function 
+
+const add = (a, b, c, d) => a + b + c + d;
+const sum = add(10, 20, 30, 40) ;
+console.log(sum);
